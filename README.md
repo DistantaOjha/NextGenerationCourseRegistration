3003d64f7a7f0797d46624c3fffb56dcf5451e36
DistantaOjha <48418897+DistantaOjha@users.noreply.github.com>
2020-03-11 13:01:56 -0400

    Refactored Base code
    Made php work
    added readme for instructions

---
+++ README.md
@@ -0,0 +1,9 @@
+To make php work on the pc
+download php.zip from php.net 
+extract
+set up php path variable
+go to this project folder on the drive
+run command php -S localhost:4000
+this will set up the php server
+go to browser and enter localhost:4000 on the browse text box
+should run index.php
---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang = "en">
+<meta charset = "UTF-8" />
+<html>
+    <head>
+        <title>Regsitration 2.0</title>
+        <link rel="stylesheet" href="css/master.css">
+        <?php
+            include("php/bootstrap.php");
+        ?>
+    </head>
+    <body>
+        <DIV class="container">
+            <DIV class="row">
+                <DIV class="col-md-12 titleBar">
+                    <H1>Welcome to Registration 2.0!</H1>
+                </DIV>
+                <DIV class="col-md-12 titleBar">
+                    <H2>What type of user are you?</H2>
+                </DIV>
+            </DIV>
+            <DIV class="row">
+                <DIV class="col-md-4 menuColumn">
+                    <form action = "studentLand.html">
+                        <button type="submit">Student</button>
+                    </form>
+                </DIV>
+                <DIV class="col-md-4 menuColumn">
+                    <form action = "instructorLand.html">
+                        <button type="submit">Instructor</button>
+                    </form>
+                </DIV>
+                <DIV class="col-md-4 menuColumn">
+                    <form action = "registrarLand.html">
+                        <button type="submit">Registrar</button>
+                    </form>
+                </DIV>
+            </DIV>
+        </DIV>
+    </body>
+</html>
