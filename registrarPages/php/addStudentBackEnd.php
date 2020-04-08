db2c0619105744db307f726f2533e2b7eec08071
duanyi02 <61850732+duanyi02@users.noreply.github.com>
2020-04-08 12:17:00 -0400

    Update addStudentBackEnd.php

---
+++ registrarPages/php/addStudentBackEnd.php
@@ -0,0 +1,43 @@
+<?php
+
+include_once("db_connect.php");
+
+function addStudent($db, $input)
+{
+
+   $fname = $input['stuFName'];
+   $mi    = $input['stuMName'];
+   $lname = $input['stuLName'];
+
+   $year  = $input['stuYear'];
+
+//select deptID
+   $major = $input['major'];
+   $minor = $input['minor'];
+
+   $res = FALSE;
+
+//insert into student table
+   $qStr1 = "INSERT INTO Students (studentID, year, fname, mi, lname) VALUES (0,'$year','$fname','$mi','$lname')";
+   $qRes1 = $db->query($qStr1);
+
+//insert students' majors into major table
+
+   $qStr2 = "INSERT INTO Majors (studentID, deptID) VALUES (0,'$major')";
+   $qRes2 = $db->query($qStr2);
+
+//insert students' minors into minor table
+
+   $qStr3 = "INSERT INTO Minors (studentID, deptID) VALUES (0,'$minor')";
+   $qRes3 = $db->query($qStr3);
+
+   if($qRes1 != FALSE && $qRes2 != FALSE && $qRes3 != FALSE)
+   {
+
+      $res = TRUE;
+
+   }
+
+   return $res;
+}
+?>
