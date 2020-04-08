228f43ca0ce7928157095eda24e3a0eaadb85bf3
DistantaOjha <48418897+DistantaOjha@users.noreply.github.com>
2020-04-08 19:28:14 -0400

    fixedAddStudent

--- registrarPages/php/addStudentBackEnd.php
+++ registrarPages/php/addStudentBackEnd.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once("../php/dbconnect.php");
+  include_once("../../php/dbconnect.php");
 
    $fname = $_POST['stuFName'];
    $mi    = $_POST['stuMName'];
@@ -13,17 +13,27 @@ include_once("../php/dbconnect.php");
    $minor = $_POST['minor'];
 
 //insert into student table
-   $qStr1 = "INSERT INTO Students (year, fname, mi, lname) VALUES ('$year','$fname','$mi','$lname')";
+   $qStr1 = "INSERT INTO Students (year, fname, mi, lname) VALUES ($year, '$fname','$mi', '$lname');";
+   print $qStr1;
    $qRes1 = $db->query($qStr1);
 
+//Get last inserted id   
+   $getLastID = "SELECT LAST_INSERT_ID();";
+   $qResGetId = $db->query($getLastID);
+   if($qResGetId != FALSE){
+     $lastInsertedRow = $qResGetId->fetch();
+     $lastInsertedId = $lastInsertedRow['0'];
+     print $lastInsertedId; 
+   }
+   
 //insert students' majors into major table
 
-   $qStr2 = "INSERT INTO Majors (deptID) VALUES ('$major')";
+   $qStr2 = "INSERT INTO Majors (studentId, deptID) VALUES ($lastInsertedId,'$major')";
    $qRes2 = $db->query($qStr2);
 
 //insert students' minors into minor table
 
-   $qStr3 = "INSERT INTO Minors (deptID) VALUES ('$minor')";
+   $qStr3 = "INSERT INTO Minors (studentID, deptID) VALUES ($lastInsertedId,'$minor')";
    $qRes3 = $db->query($qStr3);
 
    if($qRes1 != FALSE && $qRes2 != FALSE && $qRes3 != FALSE)
