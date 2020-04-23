4aa25fef0499af438648fe2121ed8ce2bc718932
DistantaOjha <48418897+DistantaOjha@users.noreply.github.com>
2020-04-22 22:37:30 -0400

    json encode fixed

--- studentPages/shoppingCartUtils/displayShoppingCart.php
+++ studentPages/shoppingCartUtils/displayShoppingCart.php
@@ -2,17 +2,14 @@
 include_once("../../php/dbconnect.php");
 $qStr = "SELECT * FROM ShoppingCart";
 $qRes = $db->query($qStr);
-$rows = array();
+
 if($qRes != FALSE)
-    $counter = 0;
     while($shopRow = $qRes->fetch()){
-        $rows[$counter]['studentID']= $shopRow['studentID'];
-        $rows[$counter]['sectionID']= $shopRow['sectionID'];
-        $rows[$counter]['placePref']= $shopRow['placePref'];
-        $counter = $counter + 1;
+        $rows = array();
+        $rows[0]= $shopRow['studentID'];
+        $rows[1]= $shopRow['sectionID'];
+        $rows[2]= $shopRow['placePref'];
+        print_r(json_encode($rows));
+        print "</br>";
     }
-    //print_r($rows);
-    //print "</br>";
-    //print "</br>";
-    print(json_encode($rows));
 ?>
