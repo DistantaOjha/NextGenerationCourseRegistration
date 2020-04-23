c841587136885a77b8b0eede7d4513d82a3b9d25
DistantaOjha <48418897+DistantaOjha@users.noreply.github.com>
2020-04-23 15:22:44 -0400

    json fix with join for seats

---
+++ studentPages/shoppingCartUtils/displayShoppingCart.php
@@ -0,0 +1,16 @@
+<?php
+include_once("../../php/dbconnect.php");
+$qStr = "SELECT studentID, sectionID, placePref, seats FROM ShoppingCart NATURAL JOIN Sections";
+$qRes = $db->query($qStr);
+
+if($qRes != FALSE)
+    while($shopRow = $qRes->fetch()){
+        $rows = array();
+        $rows[0]= $shopRow['studentID'];
+        $rows[1]= $shopRow['sectionID'];
+        $rows[2]= $shopRow['placePref'];
+        $rows[3]= $shopRow['seats'];
+        print_r(json_encode($rows));
+        print "</br>";
+    }
+?>
