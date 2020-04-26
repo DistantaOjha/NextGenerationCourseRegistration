fb1d5b5c7f12b0279be4115a04cf34f37013c4b2
isbriggs <isbriggs@users.noreply.github.com>
2020-04-26 14:02:29 -0400

    Login implementation

--- logins/studentLogin.php
+++ logins/studentLogin.php
@@ -1,11 +1,12 @@
 
-<form action="logins/php/loginStuBackEnd.php" method="POST" class = "loginForm">
+<form action="logins/php/login.php" method="POST" class = "loginForm">
     <div>
+        <input type="hidden" name="table" value="StudentUsers">
         <p class = "loginTitle">Welcome Student</p>
         </br>
         <label for="ulogin" class = "loginLabel"><b>Username</b></label>
         </br>
-        <input type="text" class= "loginInputText" placeholder="Enter your username" name="ulogin">
+        <input type="text" class= "loginInputText" placeholder="Enter your username" name="login">
         </br>
         <label for="pass" class = "loginLabel"><b>Password</b></label>
         </br>
