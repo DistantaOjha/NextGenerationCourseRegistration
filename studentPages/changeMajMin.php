2ae2eaedc83b63b375cd82c1dd908629de330e85
Ayatullah Jeter <61850773+tullahstackz@users.noreply.github.com>
2020-04-30 16:01:22 -0500

    Add files via upload

--- studentPages/changeMajMin.php
+++ studentPages/changeMajMin.php
@@ -21,38 +21,66 @@ if(!isset($_SESSION))
 
 <body>
 
-<?php
+  <?php
   include('menubar.php');
   
-  // the message
   $major = $_POST['major'];
   $minor = $_POST['minor'];
-  $username = 
-  $msg = "would like to change his/her major/minor.\nMajor Change: $major \nMinor Change: $minor";
+  $msg = "Username would like to change his/her major/minor.\nMajor Change: $major \nMinor Change: $minor";
   
-  // send email
   mail("jeteay01@gettysburg.edu","Student Major/Minor Change", $msg);
   
-?>
+  ?>
 
-<div class = "main">
-   <?php 
-      include_once("../php/dbconnect.php");
-      showUserInformation($db);
-   ?>
-</div>
+  <div class="main">
+    <DIV class="container">
+      <DIV class="row">
+        <DIV class="col-md-12">
+          <BR><BR>
+          <TABLE border="1" bordercolor="#dddddd" cellspacing="0" cellpadding="8">
+          <?php
+              $studentID = $_SESSION['username'];
+              $qStr = "SELECT * FROM Students WHERE studentID = $username;";
+              $qRes = $db->query($qStr);
+              if($qRes != False){
+                while ($row = $qRes->fetch()) {
+                  #print_r($row);
+	                $id = $row['studentID'];
+                  $year = $row['year'];
+                  $fname = $row['fname'];
+                  $mi = $row['mi'];
+                  $lname = $row['lname'];
+                  print "
+                  <TR>
+                    <TH>Name</TH>
+                    <TD>$fname $mi $lname</TD>
+                  </TR>
+                  <TR>
+                    <TH>ID</TH>
+                    <TD>$id</TD>
+                  </TR>
+                  <TR>
+                    <TH>Year</TH>
+                    <TD>$year</TD>
+                  </TR>";    
+                }  
+              }
+          ?>
+          <TR>
+            <TD>Email sent! Wait for instructor confirmation.</TD>
+            <TD>
+              <FORM name="fmMajMin" method="POST" action="studentLand.php">
+              <INPUT type="submit" value="Back to home"/>
+              </FORM>
+            </TD>
+          </TR>
+          </TABLE>
+        </DIV>
+      </DIV>
+    </DIV>
+  </div>
 
-<BR>
-
-Email sent! Wait for instructor confirmation.
-
-<FORM name="fmMajMin" method="POST" action="studentLand.php">
-
-<BR>
-
-<INPUT type="submit" value="Back to home"/>
-
-</FORM>
 
 </body>
+
 </html>
--- studentPages/studentLand.php
+++ studentPages/studentLand.php
@@ -17,6 +17,7 @@ if (!isset($_SESSION)) {
 <link rel="stylesheet" href="../css/master.css">
 <?php
 include("../php/bootstrap.php");
+include_once("../php/dbconnect.php");
 include_once("php/studentUtils.php");
 ?>
 
@@ -26,37 +27,61 @@ include_once("php/studentUtils.php");
   include('menubar.php');
   ?>
 
+
+
   <div class="main">
     <DIV class="container">
       <DIV class="row">
         <DIV class="col-md-12">
+          <BR><BR>
+          <TABLE border="1" bordercolor="#dddddd" cellspacing="0" cellpadding="8">
           <?php
-          include_once("../php/dbconnect.php");
-          showUserInformation($db, $_SESSION['username']);
+              $studentID = $_SESSION['username'];
+              $qStr = "SELECT * FROM Students WHERE studentID = $username;";
+              $qRes = $db->query($qStr);
+              if($qRes != False){
+                while ($row = $qRes->fetch()) {
+                  #print_r($row);
+	                $id = $row['studentID'];
+                  $year = $row['year'];
+                  $fname = $row['fname'];
+                  $mi = $row['mi'];
+                  $lname = $row['lname'];
+                  print "
+                  <TR>
+                    <TH>Name</TH>
+                    <TD>$fname $mi $lname</TD>
+                  </TR>
+                  <TR>
+                    <TH>ID</TH>
+                    <TD>$id</TD>
+                  </TR>
+                  <TR>
+                    <TH>Year</TH>
+                    <TD>$year</TD>
+                  </TR>";    
+                }  
+              }
           ?>
+          <TR>
+            <TD>Declare or Change Major or Minor</TD>
+            <TD>
+              <FORM name="fmMajMin" method="POST" action="changeMajMin.php">
+              <INPUT type="text" name="major" placeholder="Enter major" />
+              <BR>
+              <INPUT type="text" name="minor" placeholder="Enter minor" />
+              <BR>
+              <BR>
+              <INPUT type="submit" value="Change major/minor" />
+              </FORM>
+            </TD>
+          </TR>
+          </TABLE>
         </DIV>
       </DIV>
     </DIV>
-
   </div>
 
-  <BR>
-  <DIV class="container">
-    <DIV class="row">
-      <DIV class="col-md-12">
-        Declare/change major
-
-        <FORM name="fmMajMin" method="POST" action="changeMajMin.php">
-          <INPUT type="text" name="major" placeholder="Enter major" />
-          <BR>
-          <INPUT type="text" name="minor" placeholder="Enter minor" />
-          <BR>
-          <BR>
-          <INPUT type="submit" value="Change major/minor" />
-        </FORM>
-      </DIV>
-    </DIV>
-  </DIV>
 
 </body>
 
